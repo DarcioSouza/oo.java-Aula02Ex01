bf0087c0cb917d3d78ef32236379d0e7cb14d205
DarcioSouza <darciorsouza@gmail.com>
2023-10-31 21:56:27 -0300

    Exercício char em JAVA

---
+++ src/oo/java/Aula02Ex1.java
@@ -0,0 +1,9 @@
+package oo.java;
+
+public class Aula02Ex1 {
+    public static void main(String[] args) {
+        int i = 66;
+        char c = (char) i;
+        System.out.println("O número " + i + " corresponde à letra "+ c);
+    }
+}
